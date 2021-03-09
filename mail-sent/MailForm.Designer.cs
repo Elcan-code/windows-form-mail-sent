5fefe82ad33aee91dfb10169d22c35d01e6e0038
Elcan Seyidov <elcanseyidov1223@gmail.com>
2021-03-09 13:55:35 +0400

    Add project files.

---
+++ mail-sent/MailForm.Designer.cs
@@ -0,0 +1,327 @@
+﻿
+namespace mail_sent
+{
+    partial class MailForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.Head = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.fromtextBox = new System.Windows.Forms.TextBox();
+            this.totextBox = new System.Windows.Forms.TextBox();
+            this.SubjecttextBox = new System.Windows.Forms.TextBox();
+            this.SentButton = new System.Windows.Forms.Button();
+            this.BodytextBox = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.mailpassword = new System.Windows.Forms.Label();
+            this.passwordtextBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.Head, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label7, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(765, 596);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // Head
+            // 
+            this.Head.AutoSize = true;
+            this.Head.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Head.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold);
+            this.Head.Location = new System.Drawing.Point(156, 0);
+            this.Head.Name = "Head";
+            this.Head.Size = new System.Drawing.Size(453, 89);
+            this.Head.TabIndex = 0;
+            this.Head.Text = "MAIL-SEND";
+            this.Head.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.Controls.Add(this.label1, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label2, 1, 2);
+            this.tableLayoutPanel2.Controls.Add(this.label3, 1, 4);
+            this.tableLayoutPanel2.Controls.Add(this.label4, 1, 6);
+            this.tableLayoutPanel2.Controls.Add(this.fromtextBox, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.totextBox, 1, 3);
+            this.tableLayoutPanel2.Controls.Add(this.SubjecttextBox, 1, 5);
+            this.tableLayoutPanel2.Controls.Add(this.SentButton, 1, 10);
+            this.tableLayoutPanel2.Controls.Add(this.mailpassword, 1, 8);
+            this.tableLayoutPanel2.Controls.Add(this.passwordtextBox, 1, 9);
+            this.tableLayoutPanel2.Controls.Add(this.BodytextBox, 1, 7);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(156, 92);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 11;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(453, 411);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(93, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(265, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "From";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(93, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(265, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "To";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(93, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(265, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Subject";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(93, 168);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(265, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Body";
+            // 
+            // fromtextBox
+            // 
+            this.fromtextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.fromtextBox.Location = new System.Drawing.Point(93, 27);
+            this.fromtextBox.Name = "fromtextBox";
+            this.fromtextBox.Size = new System.Drawing.Size(265, 22);
+            this.fromtextBox.TabIndex = 4;
+            // 
+            // totextBox
+            // 
+            this.totextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.totextBox.Location = new System.Drawing.Point(93, 83);
+            this.totextBox.Name = "totextBox";
+            this.totextBox.Size = new System.Drawing.Size(265, 22);
+            this.totextBox.TabIndex = 5;
+            // 
+            // SubjecttextBox
+            // 
+            this.SubjecttextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SubjecttextBox.Location = new System.Drawing.Point(93, 139);
+            this.SubjecttextBox.Name = "SubjecttextBox";
+            this.SubjecttextBox.Size = new System.Drawing.Size(265, 22);
+            this.SubjecttextBox.TabIndex = 6;
+            // 
+            // SentButton
+            // 
+            this.SentButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SentButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SentButton.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.SentButton.Location = new System.Drawing.Point(93, 374);
+            this.SentButton.Name = "SentButton";
+            this.SentButton.Size = new System.Drawing.Size(265, 34);
+            this.SentButton.TabIndex = 7;
+            this.SentButton.Text = "SENT";
+            this.SentButton.UseVisualStyleBackColor = true;
+            this.SentButton.Click += new System.EventHandler(this.SentButton_Click);
+            // 
+            // BodytextBox
+            // 
+            this.BodytextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BodytextBox.Location = new System.Drawing.Point(93, 195);
+            this.BodytextBox.Multiline = true;
+            this.BodytextBox.Name = "BodytextBox";
+            this.BodytextBox.Size = new System.Drawing.Size(265, 117);
+            this.BodytextBox.TabIndex = 8;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel3.Controls.Add(this.label5, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.label6, 0, 1);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(156, 509);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 29.76191F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 70.2381F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(453, 84);
+            this.tableLayoutPanel3.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Enabled = false;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label5.Location = new System.Drawing.Point(3, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 18);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Warning:";
+            // 
+            // mailpassword
+            // 
+            this.mailpassword.AutoSize = true;
+            this.mailpassword.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mailpassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mailpassword.Location = new System.Drawing.Point(93, 315);
+            this.mailpassword.Name = "mailpassword";
+            this.mailpassword.Size = new System.Drawing.Size(265, 24);
+            this.mailpassword.TabIndex = 9;
+            this.mailpassword.Text = "From-Mail-Password";
+            // 
+            // passwordtextBox
+            // 
+            this.passwordtextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.passwordtextBox.Location = new System.Drawing.Point(93, 342);
+            this.passwordtextBox.Name = "passwordtextBox";
+            this.passwordtextBox.PasswordChar = '*';
+            this.passwordtextBox.Size = new System.Drawing.Size(265, 22);
+            this.passwordtextBox.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label7.Font = new System.Drawing.Font("Microsoft Uighur", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(3, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(147, 89);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Create by Elcan";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(3, 25);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(447, 59);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "From Mail only smtp.live.com (hotmail, outlook ...)";
+            // 
+            // MailForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCyan;
+            this.ClientSize = new System.Drawing.Size(765, 596);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "MailForm";
+            this.Text = "Responsive-Mail-Sent";
+            this.Load += new System.EventHandler(this.MailForm_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label Head;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox fromtextBox;
+        private System.Windows.Forms.TextBox totextBox;
+        private System.Windows.Forms.TextBox SubjecttextBox;
+        private System.Windows.Forms.Button SentButton;
+        private System.Windows.Forms.TextBox BodytextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Label mailpassword;
+        private System.Windows.Forms.TextBox passwordtextBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ mail-sent/MailForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Mail;
+namespace mail_sent
+{
+    public partial class MailForm : Form
+    {
+        public MailForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void MailForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SentButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+               
+                MailMessage message = new MailMessage();
+                SmtpClient cilent = new SmtpClient();
+                cilent.Credentials = new System.Net.NetworkCredential(fromtextBox.Text, passwordtextBox.Text);
+                cilent.Port = 587;
+                cilent.Host = "smtp.live.com";
+                cilent.EnableSsl = true;
+                message.To.Add(totextBox.Text);
+                message.From = new MailAddress(fromtextBox.Text, fromtextBox.Text);
+                message.Subject = SubjecttextBox.Text; ;
+                message.Body = BodytextBox.Text;
+                cilent.Send(message);
+                MessageBox.Show("Success");
+            }
+            catch
+            {
+                MessageBox.Show("Mail or Password invalid");
+            }
+        }
+    }
+}
